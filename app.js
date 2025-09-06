14c5e915c2abb209af319b0e19efdba5ec2b2662
Vicky <victoriaavila097@gmail.com>
2025-09-06 11:17:19 -0300

    agregue html carrito con js

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Bienvenido a tu carrito de compras");
+
+prompt("Ingresa tu usuario");
+const carrito = [];
+//VARIABLES//
+let claveUsuario = "";
+let contraseña ="123";
+let intentosClave ="0";
+
+
+
+let setAmbar;
+setAmbar= "1";
+const precio="$60000"
+console.log(setAmbar);
+
+ let colorAmbar;
+ colorAmbar="Menta";
+
+function mostrarCarrito() {
+    console.log("¡Tu carrito de compras te espera!");
+
+}mostrarCarrito(); 
+
+confirm("¿Queres continuar con la compra?")
